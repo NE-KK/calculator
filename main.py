8fff7e8a019cf8992dd8b6a450256eef26971d3f
Nils Eckmann <nils.eckmann@mein.gmx>
2025-01-20 18:40:20 +0100

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,65 @@
 # Calculator
+# import math
+import tkinter as tk
 
-calc = input("Type a calculation:\n")
+# PI = 3.1415926535
 
-result = eval(calc)
+# calc = input("Type a calculation:\n")
 
-print(result)
+# result = eval(calc)
+
+# result = round(result, 3)
+
+
+# print(result)
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("300x500")
+
+entry = tk.Entry(root, font=15)
+entry.grid(row=0, column=0, columnspan=3)
+
+# Number field ---------------------------------------------------------
+num_pad_x = 2
+num_pad_y = 10
+num_font_def = 10
+
+# Row 1 Numbers --------------------------------------------------------
+bt_1 = tk.Button(root, font=num_font_def, text="1")
+bt_1.grid(row=1, column=0, pady=num_pad_y, padx=num_pad_x)
+
+bt_2 = tk.Button(root, font=num_font_def, text="2")
+bt_2.grid(row=1, column=1, pady=num_pad_y, padx=num_pad_x)
+
+bt_3 = tk.Button(root, font=num_font_def, text="3")
+bt_3.grid(row=1, column=2, pady=num_pad_y, padx=num_pad_x)
+# Row 2 Numbers --------------------------------------------------------
+bt_4 = tk.Button(root, font=num_font_def, text="4")
+bt_4.grid(row=2, column=0, pady=num_pad_y, padx=num_pad_x)
+
+bt_5 = tk.Button(root, font=num_font_def, text="5")
+bt_5.grid(row=2, column=1, pady=num_pad_y, padx=num_pad_x)
+
+bt_6 = tk.Button(root, font=num_font_def, text="6")
+bt_6.grid(row=2, column=2, pady=num_pad_y, padx=num_pad_x)
+# Row 3 Numbers --------------------------------------------------------
+bt_7 = tk.Button(root, font=num_font_def, text="7")
+bt_7.grid(row=3, column=0, pady=num_pad_y, padx=num_pad_x)
+
+bt_8 = tk.Button(root, font=num_font_def, text="8")
+bt_8.grid(row=3, column=1, pady=num_pad_y, padx=num_pad_x)
+
+bt_9 = tk.Button(root, font=num_font_def, text="9")
+bt_9.grid(row=3, column=2, pady=num_pad_y, padx=num_pad_x)
+# Row 4 Numbers --------------------------------------------------------
+bt_0 = tk.Button(root, font=num_font_def, text="0")
+bt_0.grid(row=4, column=0, pady=num_pad_y, padx=num_pad_x)
+
+bt_p = tk.Button(root, font=num_font_def, text=".")
+bt_p.grid(row=4, column=1, pady=num_pad_y, padx=num_pad_x)
+
+
+
+
+root.mainloop()
