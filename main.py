a9e0394b539abac9e47f60dfa8521a27d1c07ba4
nils <nils.eckmann@mein.gmx>
2025-01-25 10:03:15 +0100

    with percent

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import tkinter as tk
 from tkinter import ttk
 
+#style = ttk.Style()
+#style.configure("Num_Button", font=10)
 
 def input_calculator_1():
     pos = len(entry.get())
@@ -87,7 +89,7 @@ def calculate():
     for i in range(calc_list_len):
         if calc_list[i] == "%":
             calc_list[i] = "*1/100"
-        print(calc)
+        # print(calc)
     calc = ''.join(calc_list)
     result = eval(calc)
 
@@ -99,82 +101,81 @@ def calculate():
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Calculator")
-    root.geometry("300x300")
+    root.geometry("400x300")
     root.resizable(False, False)
 
-    entry = tk.Entry(root, font=15)
+    entry = ttk.Entry(root, font=15)
     entry.grid(row=0, column=0, columnspan=4)
 
     # Number field -----------------------------------------------------------
     num_pad_x = 2
     num_pad_y = 10
-    num_font_def = 10
 
     sym_pad_y = 10
     # Row 1 Numbers ----------------------------------------------------------
-    bt_1 = tk.Button(root, font=num_font_def, text="1", command=input_calculator_1)
+    bt_1 = ttk.Button(root, text="1", command=input_calculator_1)
     bt_1.grid(row=1, column=0, pady=num_pad_y, padx=num_pad_x)
 
-    bt_2 = tk.Button(root, font=num_font_def, text="2", command=input_calculator_2)
+    bt_2 = ttk.Button(root, text="2", command=input_calculator_2)
     bt_2.grid(row=1, column=1, pady=num_pad_y, padx=num_pad_x)
 
-    bt_3 = tk.Button(root, font=num_font_def, text="3", command=input_calculator_3)
+    bt_3 = ttk.Button(root, text="3", command=input_calculator_3)
     bt_3.grid(row=1, column=2, pady=num_pad_y, padx=num_pad_x)
     # Row 2 Numbers ----------------------------------------------------------
-    bt_4 = tk.Button(root, font=num_font_def, text="4", command=input_calculator_4)
+    bt_4 = ttk.Button(root, text="4", command=input_calculator_4)
     bt_4.grid(row=2, column=0, pady=num_pad_y, padx=num_pad_x)
 
-    bt_5 = tk.Button(root, font=num_font_def, text="5", command=input_calculator_5)
+    bt_5 = ttk.Button(root, text="5", command=input_calculator_5)
     bt_5.grid(row=2, column=1, pady=num_pad_y, padx=num_pad_x)
 
-    bt_6 = tk.Button(root, font=num_font_def, text="6", command=input_calculator_6)
+    bt_6 = ttk.Button(root, text="6", command=input_calculator_6)
     bt_6.grid(row=2, column=2, pady=num_pad_y, padx=num_pad_x)
     # Row 3 Numbers ----------------------------------------------------------
-    bt_7 = tk.Button(root, font=num_font_def, text="7", command=input_calculator_7)
+    bt_7 = ttk.Button(root, text="7", command=input_calculator_7)
     bt_7.grid(row=3, column=0, pady=num_pad_y, padx=num_pad_x)
 
-    bt_8 = tk.Button(root, font=num_font_def, text="8", command=input_calculator_8)
+    bt_8 = ttk.Button(root, text="8", command=input_calculator_8)
     bt_8.grid(row=3, column=1, pady=num_pad_y, padx=num_pad_x)
 
-    bt_9 = tk.Button(root, font=num_font_def, text="9", command=input_calculator_9)
+    bt_9 = ttk.Button(root, text="9", command=input_calculator_9)
     bt_9.grid(row=3, column=2, pady=num_pad_y, padx=num_pad_x)
     # Row 4 Numbers ----------------------------------------------------------
-    bt_0 = tk.Button(root, font=num_font_def, text="0", command=input_calculator_0)
+    bt_0 = ttk.Button(root, text="0", command=input_calculator_0)
     bt_0.grid(row=4, column=0, pady=num_pad_y, padx=num_pad_x)
 
-    bt_p = tk.Button(root, font=num_font_def, text=".", command=input_calculator_p)
+    bt_p = ttk.Button(root, text=".", command=input_calculator_p)
     bt_p.grid(row=4, column=1, pady=num_pad_y, padx=num_pad_x)
 
-    bt_e = tk.Button(root, font=num_font_def, text="=", command=calculate)
+    bt_e = ttk.Button(root, text="=", command=calculate)
     bt_e.grid(row=4, column=2, pady=num_pad_y, padx=num_pad_x)
 
 
     # Row 1 Symbols ----------------------------------------------------------
-    bt_plus = tk.Button(root, font=num_font_def, text="+", command=input_calculator_plus)
+    bt_plus = ttk.Button(root, text="+", command=input_calculator_plus)
     bt_plus.grid(row=1, column=3, pady=num_pad_y, padx=num_pad_x)
 
-    bt_percent = tk.Button(root, font=num_font_def, text="%", command=input_calculator_percent)
+    bt_percent = ttk.Button(root, text="%", command=input_calculator_percent)
     bt_percent.grid(row=1, column=4, pady=num_pad_y, padx=num_pad_x)
 
     # Row 2 Symbols ----------------------------------------------------------
-    bt_minus = tk.Button(root, font=num_font_def, text="-", command=input_calculator_minus)
+    bt_minus = ttk.Button(root, text="-", command=input_calculator_minus)
     bt_minus.grid(row=2, column=3, pady=num_pad_y, padx=num_pad_x)
 
-    bt_par_open = tk.Button(root, font=num_font_def, text="(", command=input_calculator_par_open)
+    bt_par_open = ttk.Button(root, text="(", command=input_calculator_par_open)
     bt_par_open.grid(row=2, column=4, pady=num_pad_y, padx=num_pad_x)
 
     # Row 3 Symbols ----------------------------------------------------------
-    bt_mult = tk.Button(root, font=num_font_def, text="*", command=input_calculator_mult)
+    bt_mult = ttk.Button(root, text="*", command=input_calculator_mult)
     bt_mult.grid(row=3, column=3, pady=num_pad_y, padx=num_pad_x)
 
-    bt_par_close = tk.Button(root, font=num_font_def, text=")", command=input_calculator_par_close)
+    bt_par_close = ttk.Button(root, text=")", command=input_calculator_par_close)
     bt_par_close.grid(row=3, column=4, pady=num_pad_y, padx=num_pad_x)
 
     # Row 4 Symbols ----------------------------------------------------------
-    bt_div = tk.Button(root, font=num_font_def, text="/", command=input_calculator_div)
+    bt_div = ttk.Button(root, text="/", command=input_calculator_div)
     bt_div.grid(row=4, column=3, pady=num_pad_y, padx=num_pad_x)
 
-    bt_clear = tk.Button(root, font=num_font_def, text="C", command=clear)
+    bt_clear = ttk.Button(root, text="C", command=clear)
     bt_clear.grid(row=4, column=4, pady=num_pad_y, padx=num_pad_x)
 
     root.mainloop()
